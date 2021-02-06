33307618589f819c97f2edb12651d5cc8560b383
seokho <mch1273@naver.com>
2021-02-06 17:44:28 +0900

    new commit

--- CodingTest/src/Test1214.java
+++ CodingTest/src/Test1214.java
@@ -3,7 +3,7 @@ public class Test1214 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Main Master");
+		System.out.println("Main Master commit");
 	}
 
 }
