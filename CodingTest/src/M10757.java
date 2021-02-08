e841c3f72f0224d61faaf4e7506c24680cd87bad
seokho <mch1273@naver.com>
2021-02-08 23:14:29 +0900

    백준 10757 (BigInteger)

--- CodingTest/src/M10757.java
+++ CodingTest/src/M10757.java
@@ -1,9 +1,16 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class M10757 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger A = new BigInteger(sc.next());
+		BigInteger B = new BigInteger(sc.next());
+		System.out.println(A.add(B));
 	}
 
 }
