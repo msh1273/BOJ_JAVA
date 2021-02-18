70cb390f7a3a3d7b9a4e3a8d2bcf9248f632a79e
seokho <mch1273@naver.com>
2021-02-18 21:27:52 +0900

    백준 2751 (시간초과 -> 배열 중복처리 별도로 필요없었음)

--- CodingTest/src/M2751.java
+++ CodingTest/src/M2751.java
@@ -1,25 +1,27 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class M2751 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Set<Integer> sh = new HashSet<Integer>();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		Iterator<Integer> iter = null;
-		for(int i=0; i<N; i++) {
-			sh.add(Integer.parseInt(br.readLine()));		
-			iter = sh.iterator();
-			}
-		while(iter.hasNext())
-			System.out.println(iter.next());
-
+		int[] arr = new int[N];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		for(int i=0; i<arr.length; i++) {
+			bw.write(String.valueOf(arr[i]) + "\n");
+		}
+		bw.flush();
+		bw.close();
 	}
 
 }
