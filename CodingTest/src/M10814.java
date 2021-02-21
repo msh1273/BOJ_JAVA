8bc3680d8b54688bb02fdcf7f92ac9db99162b3c
seokho <mch1273@naver.com>
2021-02-21 17:31:50 +0900

    백준 10814번 2차원 배열 정렬

---
+++ CodingTest/src/M10814.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class M10814 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N =  Integer.parseInt(br.readLine());
+		String[][] person = new String[N][2];
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			person[i][0] = st.nextToken();
+			person[i][1] = st.nextToken();
+		}
+		Arrays.sort(person, (a1, a2)->{
+			
+			return Integer.parseInt(a1[0])-Integer.parseInt(a2[0]);
+			
+		});
+		for(int i=0; i<N; i++) {
+			System.out.print(person[i][0] + " " + person[i][1] + "\n");
+		}
+	}
+
+}
