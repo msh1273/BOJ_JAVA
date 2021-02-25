071b19b98e79d1b38f1df2cced548c61be0b413f
seokho <mch1273@naver.com>
2021-02-25 21:45:29 +0900

    백준 2108번 최빈값(arraylist, collections.sort)

--- CodingTest/src/M2108.java
+++ CodingTest/src/M2108.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class M2108 {
 
@@ -11,29 +13,46 @@ public class M2108 {
 		
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
-		int[] index = new int[4001];
-		int avg=0; 	//산술평균: N개의 수들의 합을 N으로 나눈 값
+		int[] index = new int[8001];
+		double sum=0; 	//산술평균: N개의 수들의 합을 N으로 나눈 값
+		int avg;
 		int centerVal;	//중앙값: N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
 		int most;	//최빈값: N개의 수들 중 가장 많이 나타나는 값
 		int range;	//범위: N개의 수들 중 최댓값과 최솟값의 차이
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-			avg+=arr[i];
+			sum+=arr[i];
 		}
+		int maxindex = 0;
+		for(int i=0; i<N; i++) {
+			index[arr[i]+4000]++;
+		}
+		for(int i=0; i<index.length; i++) {
+			if(index[i] >= maxindex) {
+				maxindex = index[i];
+			}
+		}
+		ArrayList<Integer> al = new ArrayList<>();
+		for(int i=0; i<index.length; i++) {
+			if(index[i]==maxindex) {
+				al.add(i-4000);
+			}
+		}
+
+
 		Arrays.sort(arr);
 		centerVal = arr[N/2];
-		avg=avg/N;
+		avg= (int) Math.round((sum/N));
 
-		for(int i=0; i<index.length; i++) {
-			index[arr[i]]++;
-		}
-		
-		Arrays.sort(index);
-		most = index[4000];
 		range = arr[N-1]-arr[0];
 		System.out.println(avg);
 		System.out.println(centerVal);
-		System.out.println(most);
+		Collections.sort(al);
+		if(al.size()==1) {
+			System.out.println(al.get(0));
+		}else {
+			System.out.println(al.get(1));
+		}
 		System.out.println(range);
 	}
 
